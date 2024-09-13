7f4d3443a66e0e6005476b33891809f76424e37b
rkanishka <kanishkaraghuvanshi97@gmail.com>
2024-09-12 20:23:20 -0700

    first commit

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import  login_view, logout_view
+from .views import  login_view, logout_view, about_me
 
 
 urlpatterns = [
